ddd6db00f6ccea2deeb498f37c9868dbcbcd4bd6
Leo Carten <lcarten14@gmail.com>
2024-06-03 14:20:19 -0400

    Problem 3

---
+++ problem3.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+using namespace std;
+#include<vector>
+#include<sstream>
+#include <cmath>
+
+class Problem3{
+    public:
+
+        int distance(){
+            int sum = 0;
+            int amountPeople = 0;
+            std::string input;
+            int strLength = input.size();
+            std::cout << "Please enter your input: ";
+            std::cin >> input;
+            vector<string> result;
+            stringstream s_stream(input); 
+            while(s_stream.good()) {
+                string substr;
+                getline(s_stream, substr, ','); 
+                result.push_back(substr);
+            }
+
+            int x = 0;
+            int y = 0;
+
+            for(int i = 0; i<result.size(); i++) {    
+    
+                int direction_int = (int)(result[i][1]) - 48;
+                char direction = result[i][0];
+
+                if(direction == 'N'){
+                    y += direction_int;
+                }else if(direction == 'S'){
+                    y -= direction_int;
+                }else if(direction == 'E'){
+                    x += direction_int;
+                }else{
+                    x -= direction_int;
+                }
+            }
+            return distanceFromCoordinates(x, y);
+        }
+
+        int distanceFromCoordinates(int x2, int y2){
+
+            int distance = sqrt( (pow(x2, 2)) + (pow(y2, 2)) );
+            return ceil(distance) + 1;
+
+        }
+
+};
+
+int main(){
+    Problem3 input;
+    cout << input.distance();
+    std::cout << '\n';
+    return 0;
+}
